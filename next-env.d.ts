ab82833291d122fbba80df4ba6f758c57cc0b769
Marcel <marcelmiro1@hotmail.com>
2022-03-04 11:55:01 +0000

    Add ethereum to Window interface

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,8 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+interface Window {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	ethereum: any
+}
