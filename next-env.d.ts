64a8a28f69007c1a7b542f64b45445ed4e7c2420
Marcel <marcelmiro1@hotmail.com>
2022-03-04 11:57:15 +0000

    Fix ethereum type in Window

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,8 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-interface Window {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	ethereum: any
-}
